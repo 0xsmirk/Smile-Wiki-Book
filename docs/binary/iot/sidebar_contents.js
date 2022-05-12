64e0479523b16eed2885db5733b78d61caa33520
smile-e3 <2278215957@qq.com>
2022-05-12 12:41:20 +0800

    添加:新增CVE-2021-26614_ipTIME摄像头1day分析

--- docs/binary/iot/sidebar_contents.js
+++ docs/binary/iot/sidebar_contents.js
@@ -2,9 +2,16 @@
  * @Description: IOT二进制漏洞目录
  * @Author: smile
  * @Date: 2022-05-03 15:44:42
- * @LastEditTime: 2022-05-03 16:01:31
+ * @LastEditTime: 2022-05-12 12:39:37
  * @LastEditors: smile
  */
 module.exports = [
 	"/binary/iot/",
+	{
+		title: "ipTIME",
+		collapsable: true,
+		children:[
+			"/binary/iot/ipTIME/CVE-2021-26614_ipTIME摄像头1day分析",
+		]
+	},
 ];
